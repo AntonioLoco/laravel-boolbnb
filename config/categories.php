1eacd6fb82f41956b81cf693aa76d2c40f57478c
IlCarretto <carlorebuffat412@gmail.com>
2023-02-07 17:14:42 +0100

    Add: categories file

---
+++ config/categories.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    'House', 'Flat', 'Hotel', 'Villa', 'Castle', 'Cave', 'Floating house', 'Cabin', 'Agriturism', 'Trullo', 'Camping'
+];
--- database/seeders/ApartmentSeeder.php
+++ database/seeders/ApartmentSeeder.php
@@ -14,6 +14,5 @@ class ApartmentSeeder extends Seeder
      */
     public function run()
     {
-        //
     }
 }
