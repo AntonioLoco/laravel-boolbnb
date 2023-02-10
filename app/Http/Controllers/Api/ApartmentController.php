2cd400f6034688862f2ca7c33180d6b081c96ec5
AntonioLoco <antoniolocorotondo23@gmail.com>
2023-02-10 12:48:39 +0100

    Add: query apartments for range

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -4,43 +4,47 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Apartment;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 
 class ApartmentController extends Controller
 {
     public function index(Request $request)
     {
         if ($request->has("address")) {
-            $apartments = Apartment::query();
+            //Chiamo api tomtom per latitudine e longitudine dell'indirizzo della ricerca
+            $address = $request->address;
+            $urlTomTom = "https://api.tomtom.com/search/2/geocode/" . $address . ".json?key=QEZMPbAxyM5B51twR2BRzWuWxSUDiBYg";
+            $response = Http::withOptions(['verify' => false])->get($urlTomTom);
+            $data = json_decode($response->body(), true);
+
+            //Setto latitudie e longitudine
+            $latitude = $data["results"][0]["position"]["lat"];
+            $longitude = $data["results"][0]["position"]["lon"];
 
             if ($request->has("range")) {
-                $apartments->where("Quello che mi seve");
+                $range = $request->range;
+            } else {
+                $range = 20;
             }
 
-            if ($request->has("rooms_number")) {
-                $apartments->where("Quello che mi seve");
-            }
 
-            if ($request->has("beds_number")) {
-                $apartments->where("Quello che mi seve");
-            }
-
-            if ($request->has("services")) {
-                $apartments->where("Quello che mi seve");
-            }
-
-            $apartments->get();
-
-            return response()->json([
-                "success" => true,
-                "apartments" => $apartments
-            ]);
+            $apartments = Apartment::with(['services'])
+                ->join('addresses', 'apartments.id', '=', 'addresses.apartment_id')
+                ->selectRaw("apartments.*, ( 6371 * acos( cos( radians({$latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians({$longitude}) ) + sin( radians({$latitude}) ) * sin( radians( latitude ) ) ) ) AS distance")
+                ->havingRaw("distance < {$range}")->get();
         } else {
-            $apartments = Apartment::all();
-            return response()->json([
-                "success" => true,
-                "apartments" => $apartments
-            ]);
+            $apartments = Apartment::with(['services']);
         }
+
+        $categories = Category::all();
+
+        return response()->json([
+            "success" => true,
+            "apartments" => $apartments,
+            "categories" => $categories
+        ]);
     }
 }
