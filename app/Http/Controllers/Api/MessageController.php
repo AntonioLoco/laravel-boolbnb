4d75564f122c3739ddc3ccd67c0b05367060e798
AntonioLoco <antoniolocorotondo23@gmail.com>
2023-02-15 12:56:56 +0100

    Add: Message Api

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
     public function store(Request $request)
     {
+        $request->validate([
+            "apartment_id" => "required",
+            "fullname" => "required|max:255",
+            "email" => "required",
+            "message" => "required"
+        ]);
+
+        $newMessage = Message::create([
+            "apartment_id" => $request->apartment_id,
+            "fullname" => $request->fullname,
+            "email" => $request->email,
+            "message" => $request->message
+        ]);
+
+        return response()->json([
+            "success" => true,
+            "message" => "Messaggio inviato correttamente!"
+        ]);
     }
 }
