36b5480748115fe025210a92a5b2cf3426a621d6
lawrencedulian <lawrenceedulian@gmail.com>
2023-02-20 12:34:50 +0100

    fix: ui messages, sponsor

--- resources/views/admin/apartments/sponsorship/create.blade.php
+++ resources/views/admin/apartments/sponsorship/create.blade.php
@@ -1,19 +1,17 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div id="createMessage" class="container p-5 mt-3">
-        <div class="mb-5">
-            <a href="{{ route('admin.apartments.index') }}" class="btn btn-outline-secondary">
-                Back to All
-            </a>
+    <div id="createMessage" class="container">
+        <div class="btn-back mt-3">
+            <a href="{{ route('admin.apartments.index') }}" class="btn btn-outline-secondary">Back</a>
         </div>
-        <section class="section-title text-center border-bottom border-3">
-            <h5 class="fw-ligth">SPONSORSHIP</h5>
-            <h1 class="fs-1">Do you want to sponsor the house: {{ $apartment->title }}</h1>
-            <p>Select one of the following plans</p>
+        <section class="sponsor__title text-center">
+            <p>SPONSORSHIP</p>
+            <h1>Do you want to sponsor: {{ $apartment->title }}?</h1>
+            <p class="mt-4">Select one of the following plans:</p>
         </section>
 
-        <div class="row justify-content-center mt-5 text-center">
+        <div class="row justify-content-center text-center">
             <div class="col-7">
                 @if ($errors->any())
                     <div class="alert alert-danger">
@@ -26,7 +24,7 @@
                         </ul>
                     </div>
                 @endif
-                <table class="table table-bordered">
+                <table class="table table-bordered mt-3">
                     <thead>
                         <tr class="tr_bg">
                             <th scope="col"></th>
